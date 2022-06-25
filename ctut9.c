3e2affad1a977142bea05b0ce85a32778951a6f7
anshojha <anshojha24@gmail.com>
2022-06-25 14:30:23 +0530

    Adding

---
+++ ctut9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define PI 3.14
+int main(int argc, char const *argv[])
+{
+    int a = 8;
+    const float b = 7.333;
+    b = 23;
+    
+    // printf("Hello  world!! \n");
+    // printf("The value of a is %d  and the vlaue of b is %f\n", a,b);
+    // Output - Hello  world!! 
+    // The value of a is 8  and the vlaue of b is 7.333000
+    return 0;
+}
