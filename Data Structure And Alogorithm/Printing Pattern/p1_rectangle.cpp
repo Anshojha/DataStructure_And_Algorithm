62bd15192a2c1d1ab6cd04cf18cf2d4066d29017
anshojha <anshojha24@gmail.com>
2022-06-26 21:56:09 +0530

    Pattern Printing

--- Data Structure And Alogorithm/Printing Pattern/p1_rectangle.cpp
+++ Data Structure And Alogorithm/Printing Pattern/p1_rectangle.cpp
@@ -7,14 +7,14 @@ int main()
   cin>>row; 
   cout<<"Enter the number of Column ";        
   cin>>col; 
-  for (int i = 0; i < row; i++)
+  for (int i = 1; i<=row; i++)
   {
-    for (int j = 0; j < col; j++)
+    for (int j = 1; j <= col; j++)
     {
         cout<<"*";
     }
     cout<<endl;
-  }
+  } 
   
  return 0;
 }
