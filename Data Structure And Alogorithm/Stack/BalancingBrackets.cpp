9cb6a4fa39f0955085d7122840f35b8fa32e8195
Ansh Ojha <anshojha24@gmail.com>
2023-03-09 23:21:00 +0530

    Stack

---
+++ Data Structure And Alogorithm/Stack/BalancingBrackets.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void push(int item){
+    if(top==-1){
+        
+    }
+}
+
+int main()
+{
+
+    string brc;
+    int stack[];
+    cout << "Enter Brackets :" << endl;
+
+    cin >> brc;
+
+    for (int i = 0; i <  ; i++)
+    {
+        /* code */
+    }
+    
+
+    return 0;
+}
