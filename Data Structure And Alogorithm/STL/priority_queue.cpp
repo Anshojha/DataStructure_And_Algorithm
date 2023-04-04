5d194014d005528e459f465cb5c2b33b2f485625
Ansh Ojha <anshojha24@gmail.com>
2023-04-04 23:46:18 +0530

    Queue

---
+++ Data Structure And Alogorithm/STL/priority_queue.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    // Way to create the max heap
+    //  the poped element always be the greated element among the pushed element
+    priority_queue<int> maxi;
+
+    // min heap
+
+    priority_queue<int, vector<int>, greater<int>> mini;
+
+    maxi.push(1);
+    maxi.push(5);
+    maxi.push(4);
+    int n = maxi.size();
+    cout<<"The size of maxi -> "<<maxi.size()<<endl;
+
+    for(int i=0;i<n;i++){
+        cout<<maxi.top()<<" ";
+        maxi.pop(); 
+    }cout<<endl;
+
+    cout<<"The after size of maxi -> "<<maxi.size()<<endl;
+
+    cout<<"--Its min heap--"<<endl;
+        mini.push(1);
+    mini.push(5);
+    mini.push(4);
+    int n1 = mini.size();
+    cout<<"The size of mini -> "<<mini.size()<<endl;
+
+    for(int i=0;i<n1;i++){
+        cout<<mini.top()<<" ";
+        mini.pop(); 
+    }cout<<endl;
+    cout<<"The After del size of mini -> "<<mini.size()<<endl;
+
+    return 0;
+}
--- Data Structure And Alogorithm/STL/queue.cpp
+++ Data Structure And Alogorithm/STL/queue.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 int main()
 {
-           
+     queue<string> q;
+
+     q.push("JAi");
+     q.push("Shree");
+     q.push("Ram");
+     cout<<"The size of the queue is ->"<<q.size()<<endl;
+     cout<<"Front -> "<<q.front()<<endl;
+     q.pop();
+     cout<<"Front After POP-> "<<q.front()<<endl;
+      cout<<"The size of the queue after pop ->"<<q.size()<<endl;
+
  return 0;}
