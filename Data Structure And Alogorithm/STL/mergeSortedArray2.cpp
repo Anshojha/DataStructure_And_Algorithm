44edf919f1efe041b6393a0d97939726872351d7
Ansh Ojha <anshojha24@gmail.com>
2023-05-11 23:08:01 +0530

    Merge Sorted Array

--- Data Structure And Alogorithm/STL/mergeSortedArray2.cpp
+++ Data Structure And Alogorithm/STL/mergeSortedArray2.cpp
@@ -1,15 +1,31 @@
 #include<iostream>
 using namespace std;
 
-void merge(int arr1[] , int m , int arr2[] , int n ){
-    int i=0 , j=0 ;
-
-    while(i<n && j<m){
-        if(arr1[i]<arr2[j]){
-            arr1[i++] = arr2[j++];
+void merge(int array1[], int m, int array2[], int n) {
+    int i = m - 1; // Pointer to last element in array1
+    int j = n - 1; // Pointer to last element in array2
+    int k = m + n - 1; // Pointer to last position of merged array
+    
+    // Merge the two arrays
+    while (i >= 0 && j >= 0) {
+        if (array1[i] > array2[j]) {
+            array1[k] = array1[i];
+            i--;
+        } else {
+            array1[k] = array2[j];
+            j--;
         }
+        k--;
+    }
+
+    // Add remaining elements from array2
+    while (j >= 0) {
+        array1[k] = array2[j];
+        j--;
+        k--;
     }
 }
+
 
 void display(int arr[] , int s){
     for(int i=0;i<s;i++){
@@ -23,7 +39,7 @@ int main()
 {
 
 
-int arrA[10]= {1,3,7,9};
+int arrA[10]= {1,3,7,9,0,0,0,0,0};
 int arrB[3]  ={2,5,8};
 
 
