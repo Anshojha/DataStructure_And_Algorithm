2d132b1e1f99df764a1c36fdb70843ee452fb844
Ansh Ojha <anshojha24@gmail.com>
2023-05-14 12:48:28 +0530

    String PAlindrome

---
+++ Data Structure And Alogorithm/String/checkStringPalindrome.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+char toLowerCase();
+
+void reverse(char s[] , int n){
+   int st=0;
+   int end = n-1;
+   while(st<end){
+      swap(s[st], s[end]);
+      st++;
+      end--;
+   }
+}
+bool checkStringPalindrome(char s[] , int n){
+    int e =  n-1;
+    int st = 0;
+    string org = s;
+    while(st<e){
+       if(s[st]!=s[e]){
+          return 0;
+       }
+       else{
+        st++ ;
+        e--;
+       }
+    }
+   return 1;
+}
+int main()
+{
+ int  size;
+ cout<<"Enter the size of the array ->";
+ cin>>size;
+char name[size];
+cout<<"Enter the word :"<<endl;
+cin>>name;
+cout<<"The entered word -> "<<name<<endl;
+ reverse(name, size);
+ cout<<"The reverse word -> "<<name<<endl;
+ cout<<"The given string id  palindrome if 1 and not if 0-> "<<checkStringPalindrome(name,size)<<endl;
+ return 0;}
