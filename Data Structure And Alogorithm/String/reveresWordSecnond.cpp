e9d4b0e7faa5088aea62b993f86af65578b32525
Ansh Ojha <anshojha24@gmail.com>
2023-05-17 00:03:24 +0530

    reversingWord

--- Data Structure And Alogorithm/String/reveresWordSecnond.cpp
+++ Data Structure And Alogorithm/String/reveresWordSecnond.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
-void reverseWord(char a[]){
-    
+string reverseWord(string s){
+    int l =0 , r=0;
+ while(l<s.length()){
+    while(r<s.length() && s[r]!=' '){
+        r++;
+    }
+    reverse(s.begin()+l , s.begin()+r);
+    l= r+1;
+    r=l;
+ }
+ return s;
 }
 int main()
 {
-           
+  string s = "This is the string we are taliking about";
+  cout<<reverseWord(s);
  return 0;}
