8fe6a24f7442a507b189dd74a4faea267507e1d2
Ansh Ojha <anshojha24@gmail.com>
2023-09-19 23:23:07 +0530

    OOPS concept

---
+++ Problems Practise/OOps/student.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class student{
+     private:
+     string name;
+     int age;
+     int height;
+
+     public:
+     
+     int getAge(){
+        return this->age<<endl;
+     }
+};
+
+int main()
+{
+    student s1;
+
+    
+
+    return 0;
+}
