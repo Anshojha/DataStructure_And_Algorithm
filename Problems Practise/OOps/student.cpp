d70f8edc6843118c3abd6a3ba6217319f0a3e56a
Ansh Ojha <anshojha24@gmail.com>
2023-09-19 23:36:43 +0530

    Linked List

--- Problems Practise/OOps/student.cpp
+++ Problems Practise/OOps/student.cpp
@@ -1,22 +1,54 @@
 #include <iostream>
 using namespace std;
 
-class student{
-     private:
-     string name;
-     int age;
+// class student{
+//      private:
+//      string name;
+//      int age;
+//      int height;
+
+//      public:
+     
+//      int getAge(){
+//         return this->age<<endl;
+//      }
+// };
+
+class Human{
+      public:
      int height;
+     int weight;
+     int age;
 
      public:
-     
+
      int getAge(){
-        return this->age<<endl;
+        return this->age;
      }
+
+     int setAge(int age){
+        this->age = age;
+     }
+};
+
+
+
+class Male : public Human{
+
+   public:
+   string color;
+
+   void sleep(){
+    cout<<"Male is sleeping/...."<<endl;
+   }
+
 };
 
 int main()
 {
-    student s1;
+    Male s1;
+    cout << s1.age << endl;
+    cout << s1.height << endl;
 
     
 
